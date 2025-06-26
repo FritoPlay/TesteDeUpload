7c183f92c4d2dedf32ebab2a7cec8cc033544cbc
Henrique Blem <freirehb@gmail.com>
2025-06-26 11:51:12 -0400

    Mudanças no responsive voice.

--- js-curso-2-aula_4/app.js
+++ js-curso-2-aula_4/app.js
@@ -65,7 +65,14 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
-
+if ('speechSynthesis' in window) {
+    let utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR';
+    utterance.rate = 1.2;
+    window.speechSynthesis.speak(utterance);
+} else {
+    console.log("Web Speech API não suportada neste navegador.");
+}
 
 
 
